3978a82a0476cf41929c38eacaa79672112ffa32
NathanCCH <nathan.cch@outlook.com>
2024-07-05 11:09:02 -0600

    Improve pages and update application page functionalities

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -11,7 +11,7 @@ const Profile: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gradient-to-tr from-blue-400 via-blue-100 to-blue-400">
       <div className="w-full max-w-6xl bg-white shadow-xl rounded-xl p-10">
-        <h1 className="text-5xl font-extrabold text-gray-800 mb-8">Profile</h1>
+        <h1 className="text-5xl font-extrabold text-gray-800">Profile</h1>
         <div className="flex justify-between items-center mb-8">
           {role === 'admin' && (
             <div>

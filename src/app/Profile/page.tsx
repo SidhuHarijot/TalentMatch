57c86e1d702f30e09c2c764be35b12af7cc6da3e
Yuvraj Singh Chohan <130729925+yuviChohan@users.noreply.github.com>
2024-07-28 22:48:01 -0600

    Updated Admin Profile (Khrisell)

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -11,12 +11,7 @@ const Profile: React.FC = () => {
     if (user?.is_admin) {
       return <AdminProfile />;
     } else if (user?.is_owner) {
-      return (
-        <div>
-          <h2 className="text-3xl font-bold">Owner Profile</h2>
-          <p className="text-lg">Owner side pages are under construction.</p>
-        </div>
-      );
+      return <AdminProfile/>;;
     } else {
       return <UserProfile />;
     }
